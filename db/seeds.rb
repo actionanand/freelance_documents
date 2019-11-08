89e7b51159a36c32958b25411408ab6008666e5a
actionanand <meetanandns@gmail.com>
2019-11-08 17:20:41 +0530

    changing seed for dev

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,24 +6,19 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-10.times do |d|
-	FreelanceDocument.create!(
-		title: "Document #{d}",
-		description: "This is a short description of what's written inside",
-		file_url: "https://docs.google.com/document/d/1UUgAob6ZpivgkgCbMD84JMMRc1flrzCPKybtQoAfASo/edit?usp=sharing",
-		image_url: 'https://s3.amazonaws.com/devcamp-static/images/freelance-img.jpg'
-	)
-end
+# 10.times do |d|
+# 	FreelanceDocument.create!(
+# 		title: "Document #{d}",
+# 		description: "This is a short description of what's written inside",
+# 		file_url: "https://docs.google.com/document/d/1UUgAob6ZpivgkgCbMD84JMMRc1flrzCPKybtQoAfASo/edit?usp=sharing",
+# 		image_url: 'https://s3.amazonaws.com/devcamp-static/images/freelance-img.jpg'
+# 	)
+# end
 
-10.times do |proposal|
-	Proposal.create!(
-		customer: "Customer #{proposal}",
-		portfolio_url: 'http://portfolio.jordanhudgens.com',
-		tools: 'Ruby on Rails, Angular 2, and Postgres',
-		estimated_hours: (80 + proposal),
-		hourly_rate: 120,
-		weeks_to_complete: 12,
-		client_email: 'jordan@devcamp.com',
-	)
-end
+
+FreelanceDocument.create!(title: "Rails and Angular Project", description: "In the project Ruby on Rails(RoR) is used forbackend and Angular is used in front end to make a spa", file_url: "https://docs.google.com/document/d/1UUgAob6ZpivgkgCbMD84JMMRc1flrzCPKybtQoAfASo/edit?usp=sharing", image_url: 'https://s3.amazonaws.com/devcamp-static/images/freelance-img.jpg')
+
+
+Proposal.create!(customer: "Anand Raja", portfolio_url: 'https://recipe-leaves.web.app/', tools: 'Rails 6, Angular 8, and Postgres', estimated_hours: 13, hourly_rate: 120, weeks_to_complete: 2, client_email: 'meetanandns@gmail.com')
+
 
