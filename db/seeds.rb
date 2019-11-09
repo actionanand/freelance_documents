5bfb2705d71f09778b13bb8863823f761530bfa9
actionanand <meetanandns@gmail.com>
2019-11-09 18:35:24 +0530

    seed file changed

--- db/seeds.rb
+++ db/seeds.rb
@@ -16,9 +16,9 @@
 # end
 
 
-FreelanceDocument.create!(title: "Rails and Angular Project", description: "In the project Ruby on Rails(RoR) is used forbackend and Angular is used in front end to make a spa", file_url: "https://docs.google.com/document/d/1UUgAob6ZpivgkgCbMD84JMMRc1flrzCPKybtQoAfASo/edit?usp=sharing", image_url: 'https://s3.amazonaws.com/devcamp-static/images/freelance-img.jpg')
+FreelanceDocument.create!(title: "Rails and Angular Project", description: "It's rails backend powered, Angular web app project. Charge is RS 120 per hour.", file_url: "https://docs.google.com/document/d/1oSTqg-0LIPbDlSAyqqpVM82XzKEFmNrz2IVDwAUfE8M/edit?usp=sharing", image_url: 'https://cdn.pixabay.com/photo/2014/05/02/21/49/home-office-336373_960_720.jpg')
 
 
-Proposal.create!(customer: "Anand Raja", portfolio_url: 'https://recipe-leaves.web.app/', tools: 'Rails 6, Angular 8, and Postgres', estimated_hours: 13, hourly_rate: 120, weeks_to_complete: 2, client_email: 'meetanandns@gmail.com')
+Proposal.create!(customer: "anand raja", portfolio_url: 'https://recipe-leaves.web.app/', tools: 'Rails, Angular 8, and Postgres', estimated_hours: 28, hourly_rate: 120, weeks_to_complete: 2, client_email: 'meetanandns@gmail.com')
 
 
